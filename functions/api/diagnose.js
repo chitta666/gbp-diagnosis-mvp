87221df4b438c693793dd6d7001320a2f2053ba0
Kano kyo <kanokyo@KanonoMacBook-Air.local>
2026-02-27 22:59:22 +0900

    fix diagnose

--- functions/api/diagnose.js
+++ functions/api/diagnose.js
@@ -1,16 +1,18 @@
-import * as comp from "../_lib/competitors.js";
+import comp from "../_lib/competitors.js";
 
-export async function onRequest() {
+export async function onRequest(context) {
+  const { request, env } = context;
+
+  const key = env?.GOOGLE_MAPS_API_KEY;
+  const url = new URL(request.url);
+  const lat = Number(url.searchParams.get("lat"));
+  const lng = Number(url.searchParams.get("lng"));
+
+  const competitors =
+    await comp.fetchCompetitorsAutoRadius({ key, lat, lng });
+
   return new Response(
-    JSON.stringify(
-      {
-        keys: Object.keys(comp),
-        typeof_fetchCompetitors: typeof comp.fetchCompetitors,
-        typeof_auto: typeof comp.fetchCompetitorsAutoRadius,
-      },
-      null,
-      2
-    ),
+    JSON.stringify(competitors, null, 2),
     { headers: { "content-type": "application/json" } }
   );
 }

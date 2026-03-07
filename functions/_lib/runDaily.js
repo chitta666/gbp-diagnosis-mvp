97c6adc50db2bb0318780296e7f9f6b04cd1ceaa
Kano kyo <kanokyo@KanonoMacBook-Air.local>
2026-03-07 19:34:36 +0900

    retry runDaily helper

---
+++ functions/_lib/runDaily.js
@@ -0,0 +1,28 @@
+import { ymdTokyo } from "./date.js";
+import { saveSnapshot } from "./snapshot.js";
+
+export async function runDailyForPlace({ KV, key, myPlaceId }) {
+  const rawComp = await KV.get(`comp:${myPlaceId}`);
+  if (!rawComp) {
+    return {
+      ok: false,
+      error: "competitor not set",
+      myPlaceId,
+    };
+  }
+
+  const comp = JSON.parse(rawComp);
+  const competitorPlaceId = comp.competitorPlaceId;
+
+  const my = await saveSnapshot({ KV, key, placeId: myPlaceId });
+  const competitor = await saveSnapshot({ KV, key, placeId: competitorPlaceId });
+
+  return {
+    ok: true,
+    today: ymdTokyo(),
+    myPlaceId,
+    competitorPlaceId,
+    my,
+    competitor,
+  };
+}
